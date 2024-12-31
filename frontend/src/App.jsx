22a3da3b07c8800f008c9f88a1f3215f885ff018
suthamakes <51185426+suthamakes@users.noreply.github.com>
2024-12-31 20:13:47 +0545

    organized components and minor UI changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow container mx-auto mt-8 p-4">
-        <h1 className="text-6xl text-center font-bold mb-4">\begin{"{latex}"}</h1>
-        <p className="text-2xl text-center text-gray-600">Welcome to Collaboratex.</p>
-      </main>
+      <Main />
       <Footer />
     </div>
   )
