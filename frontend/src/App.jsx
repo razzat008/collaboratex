24ad53e62c39b8e258e0ab78d297f61e522dde43
suthamakes <51185426+suthamakes@users.noreply.github.com>
2024-12-31 20:51:03 +0545

    set up basic router and page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
+import Features from "./pages/Features";
+import Templates from "./pages/Templates";
 
 function App() {
   return (
+    <Router>
     <div className="App flex flex-col min-h-screen">
-      <Navbar />
-      <Main />
+    <Navbar />
+    <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/templates" element={<Templates />} />
+        </Routes>
+      
       <Footer />
     </div>
+    </Router>
   )
 }
 
