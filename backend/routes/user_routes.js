5dd804010b22fb41563f626e9a6f6b8900c5a9fa
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-01-20 06:18:14 +0545

    added: routes, controllers
    - routes for specifying different routes
    - controller for authentication

---
+++ backend/routes/user_routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+// import User from '../model/user.js'
+import { createUser, authenticateUser } from '../controllers/user_controller.js';
+
+const router = express.Router();
+
+// router.post("/",authenticateUser);
+
+router.post("/signup", createUser);
+router.post("/login", authenticateUser);
+
+export default router;
