2ae43a1ca11ab36f8418ebef32379d908e542b2a
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-01-23 17:26:10 +0545

    delete: duplicates removed

--- src/pages/email_validate.js
+++
@@ -1,9 +0,0 @@
-export const validateEmail = (email) => {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+\.)*(ku\.edu\.np)$/;
-  if (!emailRegex.test(email)) {
-    return 'Please enter a valid email address ending with ku.edu.np';
-  }
-  return '';
-};
-
-export default validateEmail;
