3b68f6f8a1dc6532be15cffb824a794d4cd7bbc8
suthamakes <51185426+suthamakes@users.noreply.github.com>
2025-01-24 12:32:24 +0545

    changed the contents of Main component to the previous state

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,15 +1,13 @@
-// src/components/Main.jsx
-import React from 'react';
-import Table from './dashboard';  // Import Table component
-
-const Main = () => {
+function Main() {
   return (
-    <div>
-      <h1 className="text-center text-xl font-semibold mb-4">Projects Table</h1>
-      <h2> connect to the database, just the demo data </h2>
-      <Table />  {/* Render the Table component */}
-    </div>
+    <main className="flex-grow container mx-auto mt-8 p-4">
+      <h1 className="text-6xl text-center font-bold mb-4">\begin{"{latex}"}</h1><br />
+      <p className="text-2xl text-center text-gray-600">Welcome to Collaboratex.</p><br />
+      <p className="text-2xl text-center text-gray-600">Over 19 million users at research institutions and businesses worldwide use LaTex.</p><br />
+      <h2 className="text-center text-bold">The home of scientific and technical writing</h2><br />
+      <p className="text-2xl text-center text-gray-600">Create complex, beautifully formatted documents with ease<br />Collaborate with anyone, work from anywhere.</p>
+    </main>
   );
-};
+}
 
 export default Main;
