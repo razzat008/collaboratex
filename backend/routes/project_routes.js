f2991b5366869471c7d06677053c4ac6a59106ba
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-02-18 10:51:15 +0545

    Fix: routing changed, dashboard functionality added

--- backend/routes/project_routes.js
+++ backend/routes/project_routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import Project from "../model/project.js";
 import authenticateToken from "../middlewares/auth_middleware.js"; // Assuming JWT authentication
-import { createProject, findProject } from "../controllers/project_controller.js";
+import { createProject, deleteProject, findProject } from "../controllers/project_controller.js";
 
 
 const router = express.Router();
 
-router.post("/", authenticateToken, createProject);
-router.get("/", authenticateToken, findProject);
+router.post("/createProject", authenticateToken, createProject);
+router.get("/findProject", authenticateToken, findProject);
+router.post("/deleteProject", authenticateToken, deleteProject);
 
 export default router;
