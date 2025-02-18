c1da189a50028d1f004e5e76c5694ae3a0f2035e
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-02-18 12:51:06 +0545

    added: middleware to handle requests from the editorsidebar

---
+++ frontend/src/api/fileHelper.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const fetchFilesInDirectory = async (directoryPath) => {
+  try {
+    const response = await axios.get(`http://localhost:5000/api/getfiles?directory=${encodeURIComponent(directoryPath)}`, {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    });
+    // console.log("API Response:", response.data); // Log the response data
+    return response.data.files || []; // Return an empty array if 'files' is missing
+  } catch (error) {
+    console.error("Error fetching files:", error);
+    // You can also handle specific error types here and give custom messages
+    throw error; // Rethrow the error for handling in the calling function
+  }
+};
