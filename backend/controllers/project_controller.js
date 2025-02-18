f78f154769cb4fef8982c3ad0279746211d83b74
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-02-18 09:44:32 +0545

    Added: project controllers and project model

---
+++ backend/controllers/project_controller.js
@@ -0,0 +1,24 @@
+import Project from '../model/project.js';
+
+export const findProject = async (req, res) => {
+  try {
+    const projects = await Project.find({ userId: req.user.id }).sort({ createdAt: -1 });
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+
+export const createProject = async (req, res) => {
+  try {
+    const { projectName, userName, createdAt } = req.body;
+    if (!projectName) return res.status(400).json({ error: "Project name is required" });
+
+    const newProject = new Project({ userName, projectName, createdAt });
+    await newProject.save();
+    res.status(201).json(newProject);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+}
