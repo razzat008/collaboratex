cc7d5a27a936ebe4375d2c2ee913f489897314cc
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-02-22 15:24:56 +0545

    added: composite requests to update project's last modified date

--- frontend/src/api/fileHelper.js
+++ frontend/src/api/fileHelper.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Project from '../../../backend/model/project.js'
 
 export const fetchFilesInDirectory = async (directoryPath) => {
   try {
@@ -6,10 +7,10 @@ export const fetchFilesInDirectory = async (directoryPath) => {
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true,
     });
-    return response.data.files || []; 
+    return response.data.files || [];
   } catch (error) {
     console.error("Error fetching files:", error);
-    throw error; 
+    throw error;
   }
 };
 
@@ -17,36 +18,26 @@ export const fetchFileContent = async (projectId, fileName) => {
   try {
     const response = await axios.get(`http://localhost:5000/api/getfilecontent/${projectId}/${fileName}`, {
       headers: { 'Content-Type': 'application/json' },
-      withCredentials: true, 
+      withCredentials: true,
     });
-    return response.data.content; 
+    return response.data.content;
   } catch (error) {
     console.error("Error fetching file content:", error);
-    throw new Error('Failed to fetch file content'); 
+    throw new Error('Failed to fetch file content');
   }
 };
 
 export const saveLatexContent = async (projectId, fileName, latexContent) => {
-
   try {
-
-    const response = await axios.post(`http://localhost:5000/api/savelatex`, {
-      projectId,
-      fileName, 
-      content: latexContent,
-    }, {
-      withCredentials: true,
-    });
-
-
-    return response.data; 
-
+    const [content_save_response, info_update_response] = await axios.all([ //composite requests in axios
+      axios.post(`http://localhost:5000/api/savelatex`, { projectId, fileName, content: latexContent }, { withCredentials: true }),
+      axios.put(`http://localhost:5000/api/projects/update`, {
+        projectId,
+        modified_time: Date.now()
+      },
+        { withCredentials: true }),
+    ])
   } catch (error) {
-
-    console.error("Error saving LaTeX content:", error);
-
-    throw new Error('Failed to save LaTeX content'); 
-
+    console.error(error)
   }
-
 };
