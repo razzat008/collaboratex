209b74fe72f05de9a9cfb2b0624644bfbf7f10d0
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-02-22 11:14:43 +0545

    added: compile shortcuts

--- frontend/src/components/Editor/Editor.jsx
+++ frontend/src/components/Editor/Editor.jsx
@@ -1,37 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/stex/stex";
 import "./editor.css";
 import useSocket from "../../hooks/useSocket";
-import { Controlled as CodeMirror } from "react-codemirror2"; 
-import { useEffect } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
 
-export default function Editor({ value, onChange, onSave }) {
+export default function Editor({ value, onChange, onSave, onCompile }) {
   useSocket();
+
   useEffect(() => {
     const handleKeyDown = (event) => {
       if ((event.ctrlKey || event.metaKey) && event.key === "s") {
         event.preventDefault();
         console.log("Ctrl + S pressed");
-        onChange(value); 
         if (onSave) {
           onSave();
         }
       }
+      if ((event.ctrlKey || event.metaKey) && event.key === "x") {
+        event.preventDefault();
+        console.log("Ctrl + X pressed");
+        if (onCompile) {
+          onCompile();
+        }
+      }
     };
-
     document.addEventListener("keydown", handleKeyDown);
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [value, onChange, onSave]);
+  }, [onSave, onCompile]);
 
   return (
     <div className="editor h-[850px] h-full p-2 bg-gray-500">
       <CodeMirror
         className="code-mirror-wrapper h-full"
-        value={value} 
+        value={value}
         options={{
           lineWrapping: true,
           lint: false,
@@ -42,7 +47,7 @@ export default function Editor({ value, onChange, onSave }) {
           spellcheck: true,
         }}
         onBeforeChange={(editor, data, newValue) => {
-          onChange(newValue); 
+          onChange(newValue);
         }}
       />
     </div>
