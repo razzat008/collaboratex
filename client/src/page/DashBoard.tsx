c0facad9270b23240517effa51f0225f4b6dde5f
ManogyaDahal <bat16692@gmail.com>
2025-12-12 17:31:23 +0545

    add: Dashboard with just empty ui

---
+++ client/src/page/DashBoard.tsx
@@ -0,0 +1,9 @@
+import EmptyDashBoard from "../components/DashBoard/EmptyProject";
+
+export default function DashBoard() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+      <EmptyDashBoard />
+    </div>
+  );
+}
