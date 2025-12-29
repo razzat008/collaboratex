2416951ecb6021490b939b610001cd462497e082
ManogyaDahal <bat16692@gmail.com>
2025-12-29 15:33:53 +0545

    add: read function which reads from client(TS)

--- server/internal/websockets/client.go
+++ server/internal/websockets/client.go
@@ -4,7 +4,17 @@ Defines the expected behaviour of the client
 package websockets
 
 import(
+	"time"
+	"log"
+
 	"github.com/gorilla/websocket"
+)
+
+//For heartbeat 
+const ( 
+	writeWait  = 10*time.Second
+	pongWait 	 = 60*time.Second
+	pingPeriod = (pongWait * 9)/10
 )
 
 type Client struct {
@@ -17,10 +27,37 @@ type Client struct {
 }
 
 /* There is read and write operation for each clinet in seperate goroutine */ 
-/* reads the input from the client (input i.e client -> server) */
+/* reads the input from the client (Typescript)  */
 func (c *Client)Read(){ 
+	defer func(){ 
+		c.hub.unregister <- c 	
+		c.connection.Close()
+	}()	
 
+	//setting the connection's read limit to 4kb
+	// Todo: overall websocket error handeling rather than discarding data 
+	c.connection.SetReadLimit(4096) 
+	_ = c.connection.SetReadDeadline(time.Now().Add(pongWait))
+	c.connection.SetPongHandler(func(string) error { 
+		_ = c.connection.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for { 
+		//reading data from client's websocket
+		_, data, err := c.connection.ReadMessage()
+		if err != nil{ 
+			log.Printf("err:","while reading from connection")	
+		}
+
+		var document Document
+		// Todo: define The format of document and operation needs to be performed
+		//eg: for json we have to 
+
+		//Todo: broadcast
+	}
 }
+
 
 /* writes the output from server to client (i.e server -> client) */
 func (c *Client)Write(){ 
