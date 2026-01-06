85bd7c8f2d7c8c6b20a8aa79fc41673fc29d8c56
ManogyaDahal <bat16692@gmail.com>
2026-01-06 19:39:15 +0545

    update: message struct

--- server/internal/websockets/message.go
+++ server/internal/websockets/message.go
@@ -1,6 +1,18 @@
 package websockets
 
-type Document struct {
-	Content []byte
-	sender 	string
+type Message struct {
+	Type 		string				 `json:"type"`
+	Version int						 `json:"version,omitempty"`
+	Updates []ClientUpdate `json:"updates,omitempty"`
 } 
+
+type ClientUpdate struct{ 
+	ClientId string    `json:"clientId"`
+	Changes  string 	 `json:"changes"` //json rep of changes
+}
+
+type DocumentState struct{ 
+	Text string
+	Version int
+	History []ClientUpdate
+}
