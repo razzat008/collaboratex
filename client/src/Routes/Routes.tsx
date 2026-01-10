c6c1d77b200eda24d1c3beaccde3b7aeff3153ce
razzat008 <93041325+razzat008@users.noreply.github.com>
2026-01-10 19:09:22 +0545

    updated routes

--- client/src/Routes/Routes.tsx
+++ client/src/Routes/Routes.tsx
@@ -2,15 +2,31 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Playground from "../page/Playground";
 import DashBoard from "../page/DashBoard";
+import LandingLayout from "../layouts/LandingLayout";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <DashBoard /> },
-      { path: "playground", element: <Playground /> },
-      { path: "dashboard", element: <DashBoard /> },
+      { index: true, element: <LandingLayout /> }, // public landing page
+      {
+        path: "dashboard",
+        element: (
+          <ProtectedRoute>
+            <DashBoard />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "playground",
+        element: (
+          <ProtectedRoute>
+            <Playground />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ]);
