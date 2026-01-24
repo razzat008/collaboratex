8a1902ecc8de5a96425c6ff41da674ab8b6d6dc0
razzat008 <93041325+razzat008@users.noreply.github.com>
2026-01-24 09:39:07 +0545

    minor updates

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ef39e1bc.js',
+                path_to_root + 'searchindex-13883ea5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
